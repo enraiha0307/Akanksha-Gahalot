ba72897d0bcce7f32a59288f0adbd1278ead6e38
enraiha0307 <akankshagahlot0307@gmail.com>
2021-09-11 09:09:04 +0530

    second version

---
+++ app.js
@@ -0,0 +1,91 @@
+const body = document.body;
+let lastScroll = 0;
+
+window.addEventListener("scroll", () => {
+	const currentScroll = window.pageYOffset;
+	if (currentScroll <= 0) {
+		body.classList.remove("scroll-up");
+		return;
+	}
+
+	if (currentScroll > lastScroll && !body.classList.contains("scroll-down")) {
+		body.classList.remove("scroll-up");
+		body.classList.add("scroll-down");
+	} else if (
+		currentScroll < lastScroll &&
+		body.classList.contains("scroll-down")
+	) {
+		body.classList.remove("scroll-down");
+		body.classList.add("scroll-up");
+	}
+	lastScroll = currentScroll;
+});
+
+
+
+var myTween;
+function onMouseMove(event) {
+  if(myTween)
+    myTween.kill();
+  
+  mouseX = ( event.clientX / window.innerWidth ) * 2 - 1;
+  mouseY = - ( event.clientY / window.innerHeight ) * 2 + 1;
+  myTween = gsap.to(particles.rotation, {duration: 0.1, x: mouseY*-1, y: mouseX});
+  //particles.rotation.x = mouseY*-1;
+  //particles.rotation.y = mouseX;
+}
+// animate();
+
+// Scaling animation
+// var animProps = {scale: 1, xRot: 0, yRot: 0};
+// gsap.to(animProps, {duration: 10, scale: 1.2, repeat: -1, yoyo: true, ease: "sine", onUpdate: function() {
+//   renderingParent.scale.set(animProps.scale,animProps.scale,animProps.scale);
+// }});
+
+// gsap.to(animProps, {duration: 120, xRot: Math.PI * 2, yRot: Math.PI * 4, repeat: -1, yoyo: true, ease: "none", onUpdate: function() {
+//   renderingParent.rotation.set(animProps.xRot,animProps.yRot,0);
+// }});
+
+
+function animateFrom(elem, direction) {
+	direction = direction || 1;
+	var x = 0,
+		y = direction * 100;
+	if(elem.classList.contains("gs_reveal_fromLeft")) {
+	  x = -100;
+	  y = 0;
+	} else if (elem.classList.contains("gs_reveal_fromRight")) {
+	  x = 100;
+	  y = 0;
+	}
+	elem.style.transform = "translate(" + x + "px, " + y + "px)";
+	elem.style.opacity = "0";
+	gsap.fromTo(elem, {x: x, y: y, autoAlpha: 0}, {
+	  duration: 1.25, 
+	  x: 0,
+	  y: 0, 
+	  autoAlpha: 1, 
+	  ease: "expo", 
+	  overwrite: "auto"
+	});
+  }
+  
+  function hide(elem) {
+	gsap.set(elem, {autoAlpha: 0});
+  }
+  
+  document.addEventListener("DOMContentLoaded", function() {
+	gsap.registerPlugin(ScrollTrigger);
+	
+	gsap.utils.toArray(".gs_reveal").forEach(function(elem) {
+	  hide(elem); // assure that the element is hidden when scrolled into view
+	  
+	  ScrollTrigger.create({
+		trigger: elem,
+		onEnter: function() { animateFrom(elem) }, 
+		onEnterBack: function() { animateFrom(elem, -1) },
+		onLeave: function() { hide(elem) } // assure that the element is hidden when scrolled into view
+	  });
+	});
+  });
+  
